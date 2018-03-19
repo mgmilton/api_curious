4941fe2f63edde03b382dca237905ae87d35bc7a
mgmilton <matt.milton@gmail.com>
2018-03-19 15:08:45 -0600

    adds faraday connection setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
+  before_action :setup_connection
   def index
-
   end
 
   def show
@@ -8,4 +8,11 @@ class HomeController < ApplicationController
 
   def completeshow
   end
+
+  private
+    def setup_connection
+      @conn = Faraday.new(url: "https://api.github.com/") do |request|
+        request.adapter Faraday.default_adapter
+      end
+    end
 end
