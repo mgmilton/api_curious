1bea63fe0b80a97279f49d2db5f18c76c96b2e93
mgmilton <matt.milton@gmail.com>
2018-03-19 14:24:48 -0600

    adds mock github styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def show
+  end
+
+  def completeshow
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
-
+  get '/completeshow', to: "home#completeshow"
+  get '/show', to: "home#show"
   get "/auth/github", :as => :github_login
   get "/auth/github/callback", to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
