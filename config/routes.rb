f9c27c5814792bb4b559e9ff564077dfe1a10ff3
mgmilton <matt.milton@gmail.com>
2018-03-22 10:24:32 -0600

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
-  get '/completeshow', to: "home#completeshow"
   get '/show', to: "home#show"
   get "/auth/github", :as => :github_login
   get "/auth/github/callback", to: "sessions#create"
